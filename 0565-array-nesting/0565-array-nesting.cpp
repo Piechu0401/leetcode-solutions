394ba36a0a18510b2ef6c67cedad943dd8785121
Michel <michelpiech0401@gmail.com>
2026-07-21 17:41:10 +0200

    Time: 25 ms (8.26%), Space: 106.7 MB (9.64%) - LeetHub

---
+++ 0565-array-nesting/0565-array-nesting.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    inline const int arrayNesting(
+        vector<int>& __ns
+    ) const noexcept {
+        int __max{};
+        vector<char> __vs( __ns.size(), 0 );
+
+        for( int __i{}; __i < __ns.size(); ++__i ) {
+            if( __vs[__i] ) continue;
+            
+            queue<pair<int, int>> __q;
+            __vs[__i] = 1;
+            __q.push( { __i, 1 } );
+
+            while( !__q.empty() ) {
+                if( __vs[__ns[__q.front().first]] ) break;
+                else {
+                    __vs[__ns[__q.front().first]] = 1;
+                    __q.push( { __ns[__q.front().first], __q.front().second + 1 } );
+
+                }
+
+                // cout << __q.front().first << " ";
+
+                if( __max < __q.front().second )
+                    __max = __q.front().second;
+
+                __q.pop();
+
+            }
+
+            // cout << "\n";
+
+        }
+
+        return ++__max;
+        
+    }
+};

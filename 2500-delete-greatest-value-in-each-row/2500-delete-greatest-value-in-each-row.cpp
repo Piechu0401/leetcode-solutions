0910727dd1f0aa4380cb3943281d7c77066b6f66
Michel <michelpiech0401@gmail.com>
2026-08-01 14:55:00 +0200

    Time: 4 ms (23.88%), Space: 13.1 MB (51.83%) - LeetHub

--- 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.cpp
+++ 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
     inline const int deleteGreatestValue(
-        const vector<vector<int>>& __g
+        vector<vector<int>>& __g
     ) const noexcept {
-        vector<priority_queue<int>> __rs;
-
-        for( auto& __v : __g ) 
-            __rs.push_back(
-                priority_queue<int>( __v.begin(), __v.end() )
-            );
-
         int __res{};
 
-        while( !__rs.front().empty() ) {
-            int __max{};
+        while( __g.front().size() ) {
+            char __max{};
 
-            for( auto& __r : __rs ) {
-                if( __r.top() > __max ) __max = __r.top();
-                __r.pop();
+            for( auto& __v : __g ) {
+                char __localMax{};
+                char __idx{};
+
+                for( char __i{}; __i < __v.size(); ++__i ) {
+                    if( __v[__i] > __localMax ) {
+                        __localMax = __v[__i];
+                        __idx = __i;
+                        
+                    }
+
+                }
+
+                __v[__idx] = __v[__v.size() - 1];
+                __v.pop_back();
+
+                __max = ( __localMax > __max ? __localMax : __max );
 
             }
 

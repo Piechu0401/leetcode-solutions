993129e761ebac9c16b518ad51bd1dcaba7d5220
Michel <michelpiech0401@gmail.com>
2026-09-02 11:27:17 +0200

    Time: 0 ms (100%), Space: 30.3 MB (36.43%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    // what's the point of this question?
+    // the same one was where you needed to return 0
+    // I think it was about turning number to base k
+    // and checking if its a palindrome or something like that...
+
+    inline const bool uniformArray(
+        vector<int>& __ns
+    ) const noexcept { return 1; }
+};

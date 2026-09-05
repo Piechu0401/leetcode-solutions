84d67750a828279efd0c736211d7324bbe3e7e2b
Michel <michelpiech0401@gmail.com>
2026-09-05 20:54:02 +0200

    Time: 2109 ms (5.09%), Space: 115.8 MB (31.14%) - LeetHub

---
+++ 1316-distinct-echo-substrings/1316-distinct-echo-substrings.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    inline const int distinctEchoSubstrings(
+        const string& __temp
+    ) const noexcept {
+        // if you are struggling to do this, and somehow this fenomenal description 
+        // of some genius individual didn't help plus you stumbled on me reading this
+        // the question basicly wants you to find all subs that are made of some string
+        // this hints that you need to check only subs of even length plus using a hashmap
+        // to not check whole shit again, hope this clicks in for you.
+
+        string_view __t = __temp;
+        unordered_set<string_view> __us;
+        short   __size{1};
+        int     __res{};
+
+        while( __size <= __t.length() ) {
+            if( !( __size & 1 ) )
+                for( short __i{}; __i <= __t.length() - __size; ++__i ) {
+                    // string __temp{ __t.substr( __i, __size ) };
+                    char __valid{1};
+
+                    for( short __j{__i}; __j < __i + ( __size >> 1 ); ++__j )
+                        if( __t[__j] != __t[__j + ( __size >> 1 )] ) {
+                            __valid = 0;
+                            break;
+
+                        }
+
+                    if( __valid && !__us.count( __t.substr( __i, __size ) ) ) {
+                        ++__res;
+                        __us.erase( __t.substr( __i, ( __size >> 1 ) ) );
+                        __us.insert( __t.substr( __i, __size ) );
+
+                    }
+
+                }
+            else // no point in checking odd lengths
+                for( short __i{}; __i <= __t.length() - __size; ++__i )
+                    if( !__us.count( __t.substr( __i, __size ) ) )
+                        __us.insert( __t.substr( __i, __size ) );
+
+            ++__size;
+
+        }
+
+        return __res;
+        
+    }
+};
